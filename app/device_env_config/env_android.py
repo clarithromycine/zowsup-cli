36cc65801182cf53a5af68895bad8986e1e4805c
clarithromycine <is99zsq@gmail.com>
2026-05-26 11:12:09 +0800

    Update version and md5Classes in env_android.py

--- app/device_env_config/env_android.py
+++ app/device_env_config/env_android.py
@@ -22,8 +22,8 @@ class EnvAndroid:
         #在线计算地址_MD5_CLASSES, classes.dex拖进去就可以
         # https://the-x.cn/zh-cn/hash/MessageDigestAlgorithm.aspx        
 
-        self.version = "2.26.8.72"
-        self.md5Classes = "DXwhWPzRMCuFZ8AbXP4M2A=="
+        self.version = "2.26.17.72"
+        self.md5Classes = "C0PnWG0hgPtvfFkVtdTWbQ=="
         #self.key = "RFObk0NHtvEmCSluaRRbWDCd+U7QqKWi2UB4qOr/hwE+PZWmlkSqG5JGRlMsJ5+LzShVq1XyyLwWk623gAyI/w=="   
         self.key = "sdvJhddpcZ+tuNfeaKAEhS+L3M1rg7jC3ka49uKKKbOnggnuN2gUAZLlhItnagVE7d0SPOTPPplfGOowd6240Q=="
 
